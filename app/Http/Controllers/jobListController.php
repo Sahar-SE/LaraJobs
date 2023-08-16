0e78401b2cb2b9d6e4a15ef5c4d5bf8441f7dec9
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-08-16 17:14:23 +0430

    Add Controller

--- app/Http/Controllers/jobListController.php
+++ app/Http/Controllers/jobListController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class jobListController extends Controller
 {
   public function index(){
-    
+    return view('list.index');
   }
 
   public function show($id){
--- resources/views/list/index.blade.php
+++ resources/views/list/index.blade.php
@@ -6,6 +6,7 @@
     <title>Document</title>
 </head>
 <body>
+  <h1>Index Page</h1>
     
 </body>
 </html>
