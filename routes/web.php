80111e80a2ffb4bd95e2a1ff2c500f72f15c2db2
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-08-16 16:17:18 +0430

    Add Routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('list/', jobList::class, 'index')->name('list.index');
+Route::get('list/{id}', jobList::class, 'show')->name('list.show');
+
