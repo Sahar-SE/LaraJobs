a650def1cab722e018c034a9f338c934d20cd030
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-08-16 21:18:09 +0430

    Add Migration

--- database/migrations/2023_08_16_133613_create_job_lists_table.php
+++ database/migrations/2023_08_16_133613_create_job_lists_table.php
@@ -20,6 +20,7 @@ class CreateJobListsTable extends Migration
             $table->string('company');
             $table->string('location');
             $table->string('email');
+            $table->string('description');
             $table->timestamps();
         });
     }
