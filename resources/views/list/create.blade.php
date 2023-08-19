b0037ba7e5e8967386e1ad0f27845a36cc86ba37
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-08-19 18:46:54 +0430

    Add master page

--- resources/views/list/create.blade.php
+++ resources/views/list/create.blade.php
@@ -1,10 +1,11 @@
 @extends('master')
-<body>
+
+@section('contents')
+
   <form action="/list/store" method="POST" enctype="multipart/form-data">
     @csrf
     <input type="text">
     <button type="submit">sub</button>
   </form>
     
-</body>
-</html>
+@endsection
