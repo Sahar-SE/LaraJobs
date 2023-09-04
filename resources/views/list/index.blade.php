63910287121bf218198a720d8bdb24c00786a325
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-09-04 22:31:16 +0430

    Add search partial

--- resources/views/list/index.blade.php
+++ resources/views/list/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('contents')
 @include('partials._hero')
+@include('partials._search')
   @foreach($list as $list)
     <a href='list/{{$list->id}}/show'>{{$list->job_title}}</a>
     <p>{{$list->company}}</p>
