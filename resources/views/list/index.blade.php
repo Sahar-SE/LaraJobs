ed6ffef27183e8cbe7043c2ae7fe41f9137fe3a8
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-09-04 22:52:55 +0430

    Add list-card component

--- resources/views/list/index.blade.php
+++ resources/views/list/index.blade.php
@@ -4,8 +4,6 @@
 @include('partials._hero')
 @include('partials._search')
   @foreach($list as $list)
-    <a href='list/{{$list->id}}/show'>{{$list->job_title}}</a>
-    <p>{{$list->company}}</p>
-    <p>{{$list->tags}}</p>
+    
   @endforeach
 @endsection
